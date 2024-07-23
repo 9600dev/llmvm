62a4f8439de61fb3fa49b5a35df687db6c086a24
Sonny Bonds <sonny@9600.dev>
2024-07-23 16:29:06 -0600

    more refactoring

--- llmvm/client/client.py
+++ llmvm/client/client.py
@@ -1,14 +1,10 @@
 import asyncio
 import base64
-import fcntl
 import csv
 import glob
 import io
-import json
 import os
 import re
-import select
-import shlex
 import shutil
 import subprocess
 import sys
@@ -25,7 +21,6 @@ import httpx
 import jsonpickle
 import nest_asyncio
 import pyperclip
-import requests
 import rich
 from click import MissingParameter
 from click_default_group import DefaultGroup
@@ -53,8 +48,9 @@ from llmvm.common.logging_helpers import setup_logging
 from llmvm.common.objects import (Assistant, AstNode, Content, DownloadItem,
                                   Executor, FileContent, ImageContent, Message,
                                   MessageModel, PdfContent, SessionThread,
-                                  StreamNode, System, TokenStopNode, User)
+                                  StreamNode, TokenStopNode, User)
 from llmvm.common.openai_executor import OpenAIExecutor
+from llmvm.client.parsing import parse_command_string, parse_message_actions, parse_message_thread, read_from_pipe, get_path_as_messages, parse_path
 
 nest_asyncio.apply()
 
@@ -81,304 +77,6 @@ def setup_named_pipe(pid = os.getpid()):
 
 
 pipe_path = setup_named_pipe()
-
-
-async def read_from_pipe(pipe_path, timeout=0.3):
-    pipe_fd = os.open(pipe_path, os.O_RDONLY | os.O_NONBLOCK)
-    with os.fdopen(pipe_fd, 'r') as pipe:
-        buffer = ""
-        last_read_time = time.time()
-        while True:
-            try:
-                chunk = pipe.read(4096)  # Read in larger chunks
-                if chunk:
-                    buffer += chunk
-                    last_read_time = time.time()
-                else:
-                    # No new data, check if we've been idle for a while
-                    if time.time() - last_read_time > timeout and buffer:
-                        return_buffer = buffer
-                        buffer = ''
-                        yield return_buffer # Return the entire collected message
-                    await asyncio.sleep(0.1)
-            except IOError:
-                await asyncio.sleep(0.1)
-
-
-def parse_action(token) -> Content:
-    """
-    For a given [Action(...)] token, parse it into either
-    ImageContent, PdfContent, or FileContent
-    """
-    action_type = token[1:].split('(')[0]
-    action_data = Helpers.in_between(token, '(', ')')
-
-    if action_type == 'ImageContent':
-        with open(action_data, 'rb') as file_content:
-            return ImageContent(file_content.read(), url=action_data)
-    elif action_type == 'PdfContent':
-        with open(action_data, 'rb') as file_content:
-            return PdfContent(file_content.read(), url=action_data)
-    elif action_type == 'FileContent':
-        with open(action_data, 'r') as file_content:
-            return FileContent(file_content.read().encode('utf-8'), url=action_data)
-    else:
-        raise ValueError('Unknown action type')
-
-def parse_message_actions(role_type: type, message: str) -> list[Message]:
-    actions = ['[ImageContent(', '[PdfContent(', '[FileContent(']
-    accumulated_tokens = []
-    messages = []
-    # go through the message, create User() nodes for normal text content
-    # and for actions, create the appropriate action node
-    tokens = message.split(' ')
-    for token in tokens:
-        if any(token.startswith(action) for action in actions):
-            if accumulated_tokens:
-                messages.append(role_type(Content(' '.join(accumulated_tokens))))
-            messages.append(role_type(parse_action(token)))
-            accumulated_tokens = []
-        elif token:
-            accumulated_tokens.append(token)
-    if accumulated_tokens:
-        messages.append(role_type(Content(' '.join(accumulated_tokens))))
-    return messages
-
-def parse_message_thread(message: str):
-    def create_message(type) -> Message:
-        MessageClass = globals()[type]
-        return MessageClass('')
-
-    messages = []
-    roles = ['Assistant: ', 'System: ', 'User: ']
-
-    while any(message.startswith(role) for role in roles):
-        role = next(role for role in roles if message.startswith(role))
-        parsed_message = create_message(role.replace(': ', ''))
-        content = Helpers.in_between_ends(message, role, roles)
-        sub_messages = parse_message_actions(type(parsed_message), content)
-        for sub_message in sub_messages:
-            messages.append(sub_message)
-        message = message[len(role) + len(content):]
-    return messages
-
-
-def parse_path(ctx, param, value) -> List[str]:
-    def parse_helper(value, exclusions):
-        files = []
-        # deal with ~
-        item = os.path.expanduser(value)
-        # shell escaping
-        item = item.replace('\\', '')
-
-        if os.path.isdir(item):
-            # If it's a directory, add all files within
-            for dirpath, dirnames, filenames in os.walk(item):
-                for filename in filenames:
-                    files.append(os.path.join(dirpath, filename))
-
-                if not Helpers.is_glob_recursive(item):
-                    dirnames.clear()
-        elif os.path.isfile(item):
-            # If it's a file, add it to the list
-            files.append(item)
-        # check for glob
-        elif Helpers.is_glob_pattern(item):
-            # check for !
-            if item.startswith('!'):
-                for filepath in glob.glob(item[1:], recursive=Helpers.is_glob_recursive(item)):
-                    exclusions.append(filepath)
-            elif any('{' in item and '}' in item for item in value):
-                brace_items = Helpers.flatten([Helpers.glob_brace(item) for item in value if '{' in item and '}' in item])
-                files = files + brace_items
-            else:
-                for filepath in glob.glob(item, recursive=Helpers.is_glob_recursive(item)):
-                    files.append(filepath)
-        elif item.startswith('http'):
-            files.append(item)
-        return files
-
-    if not value:
-        return []
-
-    if (
-        (isinstance(value, str))
-        and (value.startswith('"') or value.startswith("'"))
-        and (value.endswith("'") or value.endswith('"'))
-    ):
-        value = value[1:-1]
-
-    files = []
-
-    if not value:
-        return files
-
-    if isinstance(value, str):
-        value = [value]
-
-    if isinstance(value, tuple):
-        value = list(value)
-
-    # see if there are any brace glob patterns, and if so, expand them
-    # and include them in the value array
-    if any('{' in item and '}' in item for item in value):
-        brace_globs = [Helpers.glob_brace(item) for item in value if '{' in item and '}' in item]
-        brace_globs = Helpers.flatten(brace_globs)
-        value += list(brace_globs)
-
-    # split by ' '
-    # value = Helpers.flatten([item.split(' ') for item in value])
-
-    exclusions = []
-
-    for item in value:
-        files.extend(parse_helper(item, exclusions))
-
-    # deal with exclusions
-    files = [file for file in files if file not in exclusions]
-    return files
-
-
-def parse_command_string(s, command):
-    def parse_option(part):
-        return next((param for param in command.params if part in param.opts), None)
-
-    try:
-        parts = shlex.split(s)
-    except ValueError:
-        parts = s.split(' ')
-
-    tokens = []
-    skip_n = 0
-
-    for i, part in enumerate(parts):
-        if skip_n > 0:
-            skip_n -= 1
-            continue
-
-        if i == 0 and part == command.name:
-            continue
-
-        # Check if this part is an option in the given click.Command
-        option = parse_option(part)
-
-        # If the option is found and it's not a flag, consume the next value.
-        if option and not option.is_flag:
-            tokens.append(part)
-            path_part = ''
-            # special case path because of strange shell behavior with " "
-            if part == '-p' or part == '--path':
-                z = i
-                while (
-                    (z + 1 < len(parts))
-                    and (
-                        parse_path(None, None, parts[z + 1])
-                        or Helpers.glob_brace(parts[z + 1])
-                        or parts[z + 1].startswith('!')
-                    )
-                ):
-                    path_part += parts[z + 1] + ' '
-                    # tokens.append(parts[z + 1])
-                    skip_n += 1
-                    z += 1
-                if path_part:
-                    path_part = path_part.strip()
-                    tokens.append(f'{path_part}')
-                else:
-                    # couldn't find any path, so display that to the user
-                    logging.debug(f'Glob pattern not found for path: {parts[z + 1]}')
-                    z += 1
-                    skip_n += 1
-                    tokens.append('')
-            else:
-                if i + 1 < len(parts):
-                    tokens.append(parts[i + 1])
-                    skip_n += 1
-        elif option and option.is_flag:
-            tokens.append(part)
-        else:
-            message = '"' + ' '.join(parts[i:]) + '"'
-            tokens.append(message)
-            break
-    return tokens
-
-
-def get_path_as_messages(
-    path: List[str],
-    upload: bool = False,
-    allowed_file_types: List[str] = []
-) -> List[User]:
-
-    files: Sequence[User] = []
-    for file_path in path:
-        # check for url
-        result = urlparse(file_path)
-
-        if (result.scheme == 'http' or result.scheme == 'https'):
-            headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}  # noqa
-            download_request = requests.get(result.geturl(), headers=headers)
-            if download_request.status_code == 200:
-                file_name = os.path.basename(result.path)
-                _, file_extension = os.path.splitext(file_name)
-                file_extension = '.html' if file_extension == '' else file_extension.lower()
-                with tempfile.NamedTemporaryFile(delete=False, suffix=file_extension) as temp_file:
-                    temp_file.write(download_request.content)
-                    temp_file.flush()
-                    file_path = temp_file.name
-                    result = urlparse(file_path)
-            else:
-                logging.debug(f'Unable to download {result.geturl()} as we got status code {download_request.status_code}')
-
-        if allowed_file_types and not any(file_path.endswith(parsable_file_type) for parsable_file_type in allowed_file_types):
-            continue
-
-        if result.scheme == '' or result.scheme == 'file':
-            if '.pdf' in result.path:
-                if upload:
-                    with open(file_path, 'rb') as f:
-                        files.append(User(PdfContent(f.read(), url=os.path.abspath(file_path))))
-                else:
-                    files.append(User(PdfContent(b'', url=os.path.abspath(file_path))))
-            elif '.htm' in result.path or '.html' in result.path:
-                try:
-                    with open(file_path, 'r') as f:
-                        file_content = f.read().encode('utf-8')
-                        # try to parse as markdown
-                        markdown = Helpers.late_bind(
-                            'helpers.webhelpers',
-                            'WebHelpers',
-                            'convert_html_to_markdown',
-                            file_content,
-                        )
-                        file_content = markdown if markdown else file_content
-                        file_content = f.read().encode('utf-8')
-                        files.append(User(FileContent(file_content, url=os.path.abspath(file_path))))
-                except UnicodeDecodeError:
-                    raise ValueError(f'File {file_path} is not a valid text file, pdf or image.')
-            elif '.pdf' in result.path:
-                if upload:
-                    with open(file_path, 'rb') as f:
-                        files.append(User(PdfContent(f.read(), url=os.path.abspath(file_path))))
-                else:
-                    files.append(User(PdfContent(b'', url=os.path.abspath(file_path))))
-            elif Helpers.classify_image(open(file_path, 'rb').read()) in ['image/jpeg', 'image/png', 'image/webp']:
-                if upload:
-                    with open(file_path, 'rb') as f:
-                        files.append(User(ImageContent(f.read(), url=os.path.abspath(file_path))))
-                else:
-                    raw_image_data = open(file_path, 'rb').read()
-                    files.append(User(ImageContent(Helpers.load_resize_save(raw_image_data), url=os.path.abspath(file_path))))
-            else:
-                try:
-                    with open(file_path, 'r') as f:
-                        if upload:
-                            file_content = f.read().encode('utf-8')
-                        else:
-                            file_content = b''
-                        files.append(User(FileContent(file_content, url=os.path.abspath(file_path))))
-                except UnicodeDecodeError:
-                    raise ValueError(f'File {file_path} is not a valid text file, pdf or image.')
-    return files
 
 
 async def stream_response(response, print_lambda: Callable) -> List[AstNode]:
@@ -517,12 +215,6 @@ async def execute_llm_call_direct(
     )
     return assistant
 
-    # messages.append(assistant)
-
-    # response_messages = list([MessageModel.from_message(m) for m in messages])
-    # result = SessionThread(id=-1, messages=response_messages)
-    # return result
-
 
 async def execute_llm_call(
     api_endpoint: str,
@@ -558,14 +250,12 @@ async def execute_llm_call(
         thread.compression = compression
 
         if mode == 'direct' or mode == 'tool' or mode == 'auto':
-            endpoint = 'tools_completions_continuation'
-        else:
-            endpoint = 'code_completions'
+            endpoint = '/tools/completions'
 
         async with httpx.AsyncClient(timeout=400.0) as client:
             async with client.stream(
                 'POST',
-                f'{api_endpoint}/v1/chat/{endpoint}',
+                f'{api_endpoint}/v1{endpoint}',
                 json=thread.model_dump(),
             ) as response:
                 objs = await stream_response(response, StreamPrinter('').write)
---
+++ llmvm/client/printing.py
@@ -0,0 +1,247 @@
+import sys
+import tempfile
+import os
+import shutil
+import subprocess
+import re
+import jsonpickle
+import async_timeout
+import asyncio
+from typing import List, Callable, cast
+
+from rich.console import Console, ConsoleOptions, RenderResult
+from rich.markdown import CodeBlock, Markdown
+from rich.syntax import Syntax
+
+from llmvm.common.helpers import Helpers
+from llmvm.common.logging_helpers import setup_logging
+from llmvm.common.objects import Message, Content, ImageContent, PdfContent, FileContent, AstNode, TokenStopNode, StreamNode, SessionThread, MessageModel
+
+
+logging = setup_logging()
+
+
+async def stream_response(response, print_lambda: Callable) -> List[AstNode]:
+    def strip_string(str):
+        if str.startswith('"'):
+            str = str[1:]
+        if str.endswith('"'):
+            str = str[:-1]
+        return str
+
+    def decode(content) -> bool:
+        try:
+            data = jsonpickle.decode(content)
+
+            # tokens
+            if isinstance(data, Content):
+                print_lambda(str(cast(Content, data)))
+            elif isinstance(data, TokenStopNode):
+                print_lambda(str(cast(TokenStopNode, data)))
+            elif isinstance(data, StreamNode):
+                print_lambda(cast(StreamNode, data))
+            elif isinstance(data, AstNode):
+                response_objects.append(data)
+            elif isinstance(data, (dict, list)):
+                response_objects.append(data)
+            else:
+                print_lambda(strip_string(data))
+            return True
+        except Exception as ex:
+            return False
+
+    response_objects = []
+    async with async_timeout.timeout(400):
+        try:
+            buffer = ''
+            async for raw_bytes in response.aiter_raw():
+                content = raw_bytes.decode('utf-8')
+                content = content.replace('data: ', '').strip()
+
+                if content == '[DONE]':
+                    pass
+                elif content == '':
+                    pass
+                else:
+                    result = decode(content)
+                    if not result:
+                        buffer += content
+                        result = decode(buffer)
+                        if result:
+                            buffer = ''
+                    else:
+                        buffer = ''
+
+        except asyncio.TimeoutError as ex:
+            logging.exception(ex)
+            # await response.aclose()
+            raise ex
+        except KeyboardInterrupt as ex:
+            await response.aclose()
+            raise ex
+    return response_objects
+
+
+
+
+class StreamPrinter():
+    def __init__(self, role: str):
+        self.buffer = ''
+        self.console = Console(file=sys.stderr)
+        self.markdown_mode = False
+        self.role = role
+        self.started = False
+
+    def display_image(self, image_bytes):
+        if len(image_bytes) < 10:
+            return
+        try:
+            # Create a temporary file to store the output from kitty icat
+            with tempfile.NamedTemporaryFile(delete=False, suffix='.jpg') as temp_file:
+                if (
+                    Helpers.is_emulator('kitty')
+                    and (
+                        shutil.which('kitty')
+                        or os.path.exists('/Applications/kitty.app/Contents/MacOS/kitty')
+                    )
+                    or (
+                        Helpers.is_emulator('tmux')
+                        and (
+                            shutil.which('kitty')
+                            or os.path.exists('/Applications/kitty.app/Contents/MacOS/kitty')
+                        )
+                        and Helpers.is_running('kitty')
+                    )
+                ):
+                    # kitty + tmux is a feature of later versions of kitty, this may not work
+                    # Use kitty icat to save its output to the temporary file
+                    cmd_path = shutil.which('kitty') or '/Applications/kitty.app/Contents/MacOS/kitty'
+                    process = subprocess.Popen(
+                        [cmd_path, 'icat', '--transfer-mode', 'file'],
+                        stdin=subprocess.PIPE,
+                        stdout=temp_file
+                    )
+                    process.communicate(input=image_bytes)
+                    process.wait()
+                    # Now cat the temporary file to stderr
+                    subprocess.run(['cat', temp_file.name], stdout=sys.stderr)
+                elif (
+                    shutil.which('viu')
+                ):
+                    temp_file.write(image_bytes)
+                    temp_file.flush()
+                    subprocess.run(['viu', temp_file.name], stdout=sys.stderr)
+        except Exception as e:
+            pass
+
+    def write_string(self, string: str):
+        if logging.level <= 20:  # INFO
+            self.console.print(f'[bright_black]{string}[/bright_black]', end='')
+
+    def write(self, node: AstNode):
+        if logging.level <= 20:  # INFO
+            if not self.started and self.role:
+                self.console.print(f'[bold green]{self.role}[/bold green]: ', end='')
+                self.started = True
+
+            string = ''
+
+            if isinstance(node, Content):
+                string = str(node)
+            elif isinstance(node, TokenStopNode):
+                string = '\n'
+            elif isinstance(node, StreamNode):
+                if isinstance(node.obj, bytes):
+                    self.display_image(node.obj)
+                    return
+            else:
+                string = str(node)
+
+            self.buffer += string
+            self.console.print(f'[bright_black]{string}[/bright_black]', end='')
+
+
+def markdown__rich_console__(
+    self,
+    console: Console,
+    options: ConsoleOptions,
+) -> RenderResult:
+    code = str(self.text).rstrip()
+    syntax = Syntax(
+        code, self.lexer_name, theme=self.theme, word_wrap=True, padding=0
+    )
+    yield syntax
+
+
+def print_response(messages: List[Message], escape: bool = False):
+    # if we're being piped or redirected, we probably want to keep the
+    # original output of the LLM, rather than render markdown.
+    def contains_token(s, tokens):
+        return any(token in s for token in tokens)
+
+    def escape_string(input_str):
+        # input_str = re.sub(r'\r?\n', '\\n', input_str)
+        # input_str = re.sub(r'\t', '\\t', input_str)
+        if escape:
+            input_str = re.sub(r'"', r'\"', input_str)
+            return input_str
+        else:
+            return input_str
+
+    def pprint(prepend: str, content: Content):
+        markdown_tokens = ['###', '* ', '](', '```', '## ']
+        console = Console()
+
+        if isinstance(content, ImageContent):
+            console.print(f'{prepend}\n', end='')
+            StreamPrinter('user').display_image(content.sequence)
+        elif isinstance(content, PdfContent):
+            CodeBlock.__rich_console__ = markdown__rich_console__
+            console.print(f'{prepend}', end='')
+            console.print(Markdown(f'[PdfContent({content.url})]'))
+        elif isinstance(content, FileContent):
+            CodeBlock.__rich_console__ = markdown__rich_console__
+            console.print(f'{prepend}', end='')
+            console.print(Markdown(f'[FileContent({content.url})]'))
+        elif contains_token(str(content), markdown_tokens) and sys.stdout.isatty():
+            CodeBlock.__rich_console__ = markdown__rich_console__
+            console.print(f'{prepend}', end='')
+            console.print(Markdown(str(content)))
+        else:
+            console.print(escape_string(f'{prepend}{content}'))
+
+    def fire_helper(string: str):
+        if 'digraph' and 'edge' and 'node' in string:
+            # fire up graphvis.
+            graphvis_code = 'digraph' + Helpers.in_between(string, 'digraph', '}') + '}\n\n'
+            temp_file = tempfile.NamedTemporaryFile(mode='w+')
+            temp_file.write(graphvis_code)
+            temp_file.flush()
+            cmd = 'dot -Tx11 {}'.format(temp_file.name)
+            subprocess.run(cmd, text=True, shell=True, env=os.environ)
+
+    for message in messages:
+        if message.role() == 'assistant':
+            temp_content = message.message
+            if type(temp_content) is Content:
+                temp_content.sequence = temp_content.get_str().replace('<code_result>', '').replace('</code_result>', '')
+            if not escape:
+                pprint('[bold cyan]Assistant[/bold cyan]: ', temp_content)
+            else:
+                pprint('', temp_content)
+            fire_helper(str(message))
+        elif message.role() == 'system':
+            if not escape:
+                pprint('[bold red]System[/bold red]: ', message.message)
+            else:
+                pprint('', message.message)
+        elif message.role() == 'user':
+            if not escape:
+                pprint('[bold cyan]User[/bold cyan]: ', message.message)
+            else:
+                pprint('', message.message)
+
+
+def print_thread(thread: SessionThread, escape: bool = False):
+    print_response([MessageModel.to_message(message) for message in thread.messages], escape)
+
